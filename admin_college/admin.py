7879c0792370cadb491dd47a4e7bbcff5ed38257
SergeyFoma <asdfg2105c@gmail.com>
2026-03-31 14:11:44 +0500

    create model teacher

--- admin_college/admin.py
+++ admin_college/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Teacher
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ['name', 'middle_name', 'first_name', 'date', 'verification_number']
 
 # Register your models here.
--- admin_college/models.py
+++ admin_college/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Teacher(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    middle_name = models.CharField(max_length=100, verbose_name='Отчество')
+    first_name = models.CharField(max_length=100, verbose_name='Фамилия')
+    date = models.DateTimeField(auto_now=True, verbose_name='Дата создания')
+    verification_number = models.CharField(max_length=150, verbose_name='Верификационный номер')
+
+    class Meta:
+        verbose_name = 'Преподаватель'
+        verbose_name_plural = 'Преподаватели'
