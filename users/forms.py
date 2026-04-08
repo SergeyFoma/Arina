d59a42de53cffb771d344ec1954a9a6077833b0b
SergeyFoma <asdfg2105c@gmail.com>
2026-04-08 16:17:31 +0500

    wsd

--- users/forms.py
+++ users/forms.py
@@ -6,6 +6,7 @@ from django.contrib.auth import get_user_model
 #from django.contrib.auth.models import User
 
 CHOICES = [
+    ('status','Status'),
     ('student', 'Студент'),
     ('teacher', 'Преподаватель'),
     ('admin', 'Администратор'),
