945851f7901fcb932ae24af2f8e868b7b37fa8d6
SergeyFoma <asdfg2105c@gmail.com>
2026-04-09 10:44:09 +0500

    'asd'

--- admin_college/views.py
+++ admin_college/views.py
@@ -1,33 +1,46 @@
 from django.shortcuts import render
 from admin_college.models import Teacher
 from django.shortcuts import redirect
+from django.http import HttpResponse
+from django.contrib import messages
 
 def check(request):
     #print(request.GET)
-    a=request.GET
-    #print('Value: ', a.value)
-    te=[]
-    teach=Teacher.objects.all()
-    #print(teach)
-    for t in teach:
-        te.append(t.verification_number)
-    print('T: ', te)
+    # a=request.GET
+    # #print('Value: ', a.value)
+    # te=[]
+    # teach=Teacher.objects.all()
+    # #print(teach)
+    # for t in teach:
+    #     te.append(t.verification_number)
+    # print('T: ', te)
 
-    vv=''
-    for k,v in a.items():
-        vv=v
-        print('V: ', vv)
-    if vv in te:
-        print('YES: ', vv)
+    # vv=''
+    # for k,v in a.items():
+    #     vv=v
+    #     print('V: ', vv)
 
-    if vv in te:
-        return redirect('users:login_users')
+    # if vv in te:
+    #     return redirect('users:login_users')
 
+    if request.method=='POST':
+        check = request.POST.get('check','')
+        print(check)
+        te=[]
+        teach=Teacher.objects.all()
+        for t in teach:
+            te.append(t.verification_number)
+        
+        if check in te:
+            return redirect('users:login_users')
+        else:
+            messages.success(request, 'Не верный номер')
+            return redirect('admin_college:check')
     
     context={
-        'teach':teach,
-        'te':te,
-        'vv':vv,
+        # 'teach':teach,
+        # 'te':te,
+        # 'vv':vv,
     }
     return render(request,"admin_college/check.html", context)
 
